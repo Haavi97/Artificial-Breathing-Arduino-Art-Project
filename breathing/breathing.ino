be57a94151150c6dd089fddd3fd4daa1e0191eff
fortin <fortin@ttu.ee>
2021-02-04 17:45:32 +0200

    Valve control planned

--- breathing/breathing.ino
+++ breathing/breathing.ino
@@ -2,7 +2,7 @@
  * File:        breathing.ino
  * Author:      Francisco Javier Ortín Cervera
  * Created:     21.01.2021
- * Last edit:   29.12.2021
+ * Last edit:   04.02.2021
  *
  * Description: Program for representing the breathing of
  *              a living organism using a compressor 
@@ -13,9 +13,14 @@
  * 
   */
 
-int const compressor = 4;
+#include <Servo.h>
+
 int const potentiometer = A1;
+int const compressor = 4;
 int const button = 5;
+int const servo = 6;
+
+Servo myservo;
 
 int control = 0;
 // 0 controls input time
@@ -25,21 +30,34 @@ int after_control = 200;
 // Time (ms) to way after pressing control button
 
 int time_read = 1000;
-int factor = 4; 
+int factor = 8; 
 // Factor to multiply the analog read time so
 // if factor is 4 then the maximum time is 4*1023
 
-int input_time = 500; 
+int filling_time = 20000;
+// Initial time to fill before the breathing
+int input_time = 5000; 
 // Default time the compressor is turned on
-int rest_time = 1000;
+int rest_time = 5000;
 // Default time the compressor is turned off
+
+int closed_angle = 90;
+// initial angle at which the valve is closed
+
+int open_angle = 10;
+// angle at which the valve is open
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(compressor, OUTPUT);
   pinMode(potentiometer,INPUT);
   pinMode(button, INPUT);
+
+  myservo.attach(servo);
+  
   digitalWrite(compressor, LOW);
+  
+  myservo.write(closed_angle);
 
   Serial.begin(9600);
   // Serial com for debugging
@@ -61,8 +79,10 @@ void loop() {
   else        input_time = time_read;
   
   digitalWrite(compressor, HIGH);
+  myservo.write(open_angle);
   delay(input_time);
   digitalWrite(compressor, LOW);
+  myservo.write(closed_angle);
   delay(rest_time);
   
 }
